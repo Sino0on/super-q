d093471f9db3a7a27ef4e301f9cc0e525dd28dfd
Dastan <95084961+Sino0on@users.noreply.github.com>
2025-07-08 01:01:43 +0600

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -145,23 +145,23 @@ CKEDITOR_CONFIGS = {
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': config('POSTGRES_DB'),
+#        'USER': config('POSTGRES_USER'),
+#        'PASSWORD': config('POSTGRES_PASSWORD'),
+#        'HOST': config('POSTGRES_HOST'),
+#        'PORT': config('POSTGRES_PORT', cast=int),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('POSTGRES_DB'),
-        'USER': config('POSTGRES_USER'),
-        'PASSWORD': config('POSTGRES_PASSWORD'),
-        'HOST': config('POSTGRES_HOST'),
-        'PORT': config('POSTGRES_PORT', cast=int),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / "db.sqlite3",
-#     }
-# }
 
 
 # Password validation
